cc6f1c54225419eebb0e949203ea6ab019173a4b
Ivan Romashenko <romashenko.smart@gmail.com>
2025-06-04 20:36:08 +0300

    init commit

---
+++ src/test/java/org/example/testgitmaster/TestGitMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.testgitmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestGitMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
